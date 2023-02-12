d10c4218f2e4d7377ceb07d9ea3c01da1b571d93
nveeraboyani <89230085+nveeraboyani@users.noreply.github.com>
2023-02-12 00:07:12 -0800

    sg

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [aws_security_group.blog.id]
-  
+
 
   tags = {
     Name = "HelloWorld"
@@ -34,7 +34,7 @@ resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow HTTP & HTTPS in. Allow everything out"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_security_group_ids = [aws_security_group.blog.id]
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
